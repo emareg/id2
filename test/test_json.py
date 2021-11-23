f3567028ff26dbcb25a46b8148521c65d1f17160
emareg <emanuel.regnath@tum.de>
2021-11-23 16:00:34 +0100

    add test for JSON format

---
+++ test/test_json.py
@@ -0,0 +1,31 @@
+import json
+import re
+import sys
+
+from src.id2 import id2data
+
+
+def isUrlOk(url):
+    return url == "" or url[0:8] == "https://" or url[0:7] == "mailto:"
+
+
+
+def testJSON():
+    isError = False
+    for key, entry in id2data.items():
+        print("{}: ".format(key), end=" ", flush=True)
+        if key == entry["id"] and len(entry) == 7 and entry["desc"] != "" and isUrlOk(entry["url"]) and re.match(r'[0-9,-]+', entry["len"]):
+            print("OK.")
+        else:
+            print("ERROR!\n=========\n", str(entry))
+            isError = True
+
+    if isError:
+        print("\nThere have been errors.")
+        sys.exit(1)
+    else:
+        print("\nAll {} entries OK.".format(len(id2data)))
+
+
+
+testJSON()
