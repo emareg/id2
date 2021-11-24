781c48a0488a9d129fdc4eafb9fc1b18b89b2787
emareg <emanuel.regnath@tum.de>
2021-11-24 20:11:10 +0100

    add time profile

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,14 +1,25 @@
 import json
 import re
 import sys
+import cProfile, pstats
 
 from src.id2 import guessId, id2data
 
+
+prof = cProfile.Profile()
+
+
 def testExamples():
+    guessId('blabla')   # first burn
+
     isError = False
     for key, entry in id2data.items():
         print("key:", key, entry['ex'], end=" ", flush=True)
+
+        prof.enable()
         types = guessId(entry['ex'])
+        prof.disable()
+
         if key in [x['id'] for x in types]:
             print("PASSED ({})".format(len(types)))
         else:
@@ -16,8 +27,11 @@ def testExamples():
             isError = True
 
     if isError:
+        print("\nThere have been errors.")
         sys.exit(1)
-
+    else:
+        print("\nAll {} entries OK.".format(len(id2data)))
 
 
 testExamples()
+pstats.Stats(prof).print_stats('id2.py')
